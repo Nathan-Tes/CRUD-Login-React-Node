ccc0d242032a2d915cdf13cca563d0c178a6a18d
Nathan-Tes <natesstrab@gmail.com>
2024-03-25 10:37:21 -0300

    criacao de autenticacao de user

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,26 @@
 const express = require("express");
 
 const routes = express.Router();
+const users = [
+  {
+    id: 1,
+    name: "Lucas",
+    email: "contactlucas@gmail.com",
+    password: "12345678",
+  },
+];
 
 routes.post("/login", (req, res) => {
   const { email, password } = req.body;
-  res.send(email);
+
+  const user = users.find(
+    user => user.email === email && user.password === password
+  );
+  if (user) {
+    return res.status(200).json(user);
+  }
+
+  return res.status(401).json({ message: "Invalid Credentials" });
 });
 
 module.exports = routes;
